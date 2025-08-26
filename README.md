24a020e18f0e835f1a57d0645187f2be1c068430
Roney Dsilva <roney.dsilva@cdmx.in>
2025-08-26 16:59:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# wappler_opensearch_logs
+
+Instructions:
+1. Set the environment variables in the config.json or user_config.json file whichever is defined
+OPS_USERNAME - opensearch Instance Username
+OPS_PASSWORD - opensearch Instance Password
+DOMAIN - for the domain in the logs object so as to be able to distinguish the logs based on domains
+2. Add the module to which API you wish to send the logs from to Opensearch.
+
+
+Example:
+![image](https://github.com/roney492/wappler_opensearch_logs/assets/116948023/912006c4-485c-4aa5-9264-019980545755)
+![image](https://github.com/roney492/wappler_opensearch_logs/assets/116948023/d018715a-7bf1-4759-ac41-996f3d52b6e5)
+
---
+++ server_connect/opensearch-query.js
@@ -0,0 +1,107 @@
+require('dotenv').config();
+const { Client } = require('@opensearch-project/opensearch');
+
+const opsUser = process.env.OPS_USERNAME;
+const opsPass = process.env.OPS_PASSWORD;
+const opsUrl = process.env.OPS_URL;
+
+exports.ops_query = async function (options) {
+  const timestampField = this.parse(options.timestampField) || '@timestamp';
+  const sortField = this.parse(options.sort_field);
+  const sortOrder = this.parse(options.sort_order) || 'desc';
+  const opensearchConfig = {
+    node: opsUrl,
+    auth: {
+      username: opsUser,
+      password: opsPass,
+    },
+    ssl: {
+      rejectUnauthorized: options.ssl_check ? true : false,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const opensearchClient = new Client(opensearchConfig);
+  const indexName = this.parse(options.index);
+
+  const body = {
+    query: {
+      bool: {
+        must: [
+          // Remove the match part if options.field or options.query is missing
+          options.field && options.query
+            ? {
+                match_phrase: {
+                  [this.parse(options.field)]: this.parse(options.query),
+                },
+              }
+            : undefined,
+          options.fromTimestamp && options.toTimestamp
+            ? {
+                range: {
+                  [timestampField]: {
+                    gte: this.parse(options.fromTimestamp),
+                    lte: this.parse(options.toTimestamp),
+                  },
+                },
+              }
+            : undefined,
+            options.is_note ? 
+            {
+              term: {
+                is_note: this.parse(options.is_note),
+              },
+            }
+            : undefined,
+        ].filter(Boolean), 
+      },
+    },
+    sort: sortField
+      ? [{ [sortField]: sortOrder }]
+      : undefined,
+  };
+  
+  if (options.output_fields) {
+    body._source = this.parse(options.output_fields).split(',');
+  }
+  try {
+    const initialResponse = await opensearchClient.transport.request({
+      method: 'POST',
+      path: '/_search',
+      querystring: {
+        scroll: '1m',
+        size: 1000,
+      },
+      body: body,
+    });
+    let hits = initialResponse.body.hits.hits.map(hit => hit._source);
+    let scrollId = initialResponse.body._scroll_id;
+
+    while (hits.length < initialResponse.body.hits.total.value) {
+      const scrollResponse = await opensearchClient.transport.request({
+        method: 'POST',
+        path: '/_search/scroll',
+        body: {
+          scroll: '1m',
+          scroll_id: scrollId,
+        },
+      });
+
+      hits = hits.concat(scrollResponse.body.hits.hits.map(hit => hit._source));
+      scrollId = scrollResponse.body._scroll_id;
+    }
+
+    await opensearchClient.transport.request({
+      method: 'DELETE',
+      path: '/_search/scroll',
+      body: { scroll_id: scrollId },
+    });
+
+    return hits;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+}
